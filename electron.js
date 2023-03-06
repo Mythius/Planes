d88d551642e7a3937a7f96771fad2992a4645310
Matthias Southwick <southwickmatthias@gmail.com>
2023-03-05 21:39:12 -0700

    Added Library Files

---
+++ electron.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron');
+
+const name = "";
+
+
+
+function createWindow () {
+  let win = new BrowserWindow({
+    width: 600,
+    height: 400,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    icon: __dirname + '/site/icon.png'
+  })
+
+  win.loadFile('site/index.html')
+
+  win.setMenu(null);
+}
+
+app.on('ready', createWindow)
+
+/*
+
+  package.json
+
+  "build":{
+    "win": {
+      "icon": "site/icon.png"
+    }
+  }
+
+
+*/
